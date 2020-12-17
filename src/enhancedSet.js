ecbb67a45e727c628380aadf7d2c14f5fb137f48
Eyas Ranjous <eyas.ranjous@gmail.com>
2020-12-18 00:56:21 +0100

    add permutations

--- src/enhancedSet.js
+++ src/enhancedSet.js
@@ -140,16 +140,59 @@ class EnhancedSet extends Set {
    * @public
    * projecting the power concept into Set self Cartesian Product
    * @param {number} m
+   * @param {string} separator
    * @returns {EnhancedSet}
    */
   power(m, seprator = '') {
-    if (m === 0) return new EnhancedSet();
+    if (Number.isNaN(+m) || +m < 0) {
+      throw new Error('.power expects a positive number');
+    }
+
+    if (+m === 0) return new EnhancedSet();
 
     let result = this.clone();
-    for (let i = 0; i < m - 1; i += 1) {
+    for (let i = 0; i < +m - 1; i += 1) {
       result = result.product(this, seprator);
     }
 
+    return result;
+  }
+
+  /**
+   * @public
+   * finds m permutations of the set
+   * @param {number} m
+   * @param {string} separator
+   * @returns {EnhancedSet}
+   */
+  permutations(m, separator = '') {
+    if (Number.isNaN(+m) || +m < 0) {
+      throw new Error('.permutations expects a positive number');
+    }
+
+    if (m > this.size) {
+      throw new Error('.permutations expects a number less or euqal set size');
+    }
+
+    const result = new EnhancedSet();
+
+    const generatePermutation = (currentSet, i = 0, prefix = '') => {
+      if (i === m && prefix.length > 0) {
+        result.add(prefix);
+        return;
+      }
+
+      currentSet.forEach((element) => {
+        const nextSet = currentSet.clone();
+        nextSet.delete(element);
+        const accumulated = prefix.length
+          ? `${prefix}${separator}${element}`
+          : element;
+        generatePermutation(nextSet, i + 1, accumulated);
+      });
+    };
+
+    generatePermutation(this.clone());
     return result;
   }
 
